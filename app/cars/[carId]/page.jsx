6fa2751e7d96c99dd83e01d0000b53c5a296bec8
Ebrahimim29 <Ebrahimim29@gmail.com>
2026-06-21 06:41:33 +0330

    Server Component-Client Component

--- app/cars/[carId]/page.jsx
+++ app/cars/[carId]/page.jsx
@@ -1,13 +1,17 @@
-export const generateMetadata = ({ params, searchParams })=>{
-    return {
-        title: `car no ${params.carId}`,
-    }
-}
+'use client'
+
+// export const generateMetadata = ({ params, searchParams })=>{
+//     return {
+//         title: `car no ${params.carId}`,
+//     }
+// }
 
 const Page = ({ params, searchParams }) => {
+    console.log(params);
+    
     return (
         <section className="featured-cars" style={{width:"100%",display:"flex", flexDirection:"column", marginTop:"70px", justifyContent:"center", alignItems:"center"}}>
-            <div className="col-lg-3 col-md-4 col-sm-6" >
+            <div className="col-lg-3 col-md-4 col-sm-6" onClick={()=>{console.log(searchParams);}} >
                 <h1>
                     Car No:
                     {params.carId}
@@ -27,7 +31,7 @@ const Page = ({ params, searchParams }) => {
                         </div>
                     </div>
                     <div className="featured-cars-txt">
-                        <h2><a href="#">BMW 6-series gran coupe - {params.carId}</a></h2>
+                        <h2><a href="#">Car Name :{searchParams.title} - {params.carId}</a></h2>
                         <h3>{params.carId}</h3>
                         <p>
                             Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non.
