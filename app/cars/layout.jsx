476135f3dfd296e18805d15b4a919a57606b0864
Ebrahimim29 <Ebrahimim29@gmail.com>
2026-06-27 08:15:07 +0330

    Template

--- app/cars/layout.jsx
+++ app/cars/layout.jsx
@@ -1,37 +1,14 @@
 export const metadata = {
-  title: 'Cars',
+    title: 'cars',
 }
-
 
 const CarsLayout = ({children}) => {
     return(
         <>
-            
             {children}
-
-            <div 
-                style={{
-                    backgroundColor:"blue",
-                    width:"10rem",
-                    cursor:"pointer",
-                    height:"10rem",
-                    position:"fixed",
-                    top:"20rem",
-                    left:"20rem",
-                    borderRadius:"10rem",
-                    display:"flex",
-                    justifyContent:"center",
-                    alignItems:"center",
-                    color:"white",
-                    flexDirection:"column",
-                    gap:"1rem",
-                    fontSize:"2rem"}} >
-                
-                <i className="fa fa-shopping-basket"></i>
-                کارت من
-            </div>
         </>
-    )
-};
+    );
+}
+
 
 export default CarsLayout;
